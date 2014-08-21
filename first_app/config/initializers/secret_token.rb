9ec73b4c925624c19f8519a961a145aaea75c9ad
Dave Lin <David_Lin@comcast.com>
2014-08-20 20:53:40 -0400

    Initialize Repository

---
+++ first_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '2d0783ea1dbf5804f207d932cee2972814d181fc50fed387dbbf9f6d23c16ea871e213b8732c9949067f50c2da24e90ac55e480b11a52b08d1782e40c099d1a8'
